5ca2ba0fb1dc9304327ba4f2b7c172d1407cd1a9
redderpanda <jaredsc@uci.edu>
2018-02-05 16:09:20 -0800

    pushing js again
    ????

--- Pong.js
+++ Pong.js
@@ -4,3 +4,49 @@ var height = 600;
 canvas.width = width;
 canvas.height = height;
 var context = canvas.getContext('2d');
+
+
+var conn;
+
+var animate =  window.requestAnimationFrame ||
+window.webkitRequestAnimationFrame ||
+window.mozRequestAnimationFrame ||
+function(callback) {window.setTimeout(callback,1000/60)};
+
+window.onload = function(){
+	document.body.appendChild(canvas);
+	animate(step);
+};
+
+var step = function(){
+	update();
+	render();
+	animate(step);
+};
+
+var update = function(){
+};
+
+var render = function(){
+	context.fillStyle = "#00FBF2";
+	context.fillRect(0,0,width,height);
+};
+
+this.disconnect = function() {
+	this.conn.close();
+};
+
+this.connect = function() {
+	if ( typeof(MozWebSocket) == 'function' )
+		this.conn = new MozWebSocket(url);
+	else
+		this.conn = new WebSocket(url);
+
+	// dispatch to the right handlers
+	this.conn.onmessage = function(evt){
+		dispatch('message', evt.data);
+	};
+
+	this.conn.onclose = function(){dispatch('close',null)}
+	this.conn.onopen = function(){dispatch('open',null)}
+};
